a67ab181d83eb56fe692bccc12a09ea9d117e62f
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-07-04 16:55:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from Model import model
 
 st.title("Chatbot")
 
-user_input = st.text_input("User Input")
+user_input = st.text_input("Ask here:")
 
 if st.button("Send"):
     response = model(user_input)
+    styled_user_input = f'<p style="color:green; text-align:right;">{user_input}</p>'
+    st.markdown(styled_user_input, unsafe_allow_html=True)
     st.text(response)
