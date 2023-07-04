6e220f686f68f3ac41b1d186f91f788df77e48aa
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-07-04 17:05:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from Model import model
 
+# https://discuss.streamlit.io/t/1-st-button-delete-result-of-2-nd-button/33280/2
+if 'log' not in st.session_state:
+    st.session_state.log = []
 
 st.title("Chatbot")
 
@@ -9,5 +12,13 @@ user_input = st.text_input("Ask here:")
 if st.button("Send"):
     response = model(user_input)
     styled_user_input = f'<p style="color:green; text-align:right;">{user_input}</p>'
-    st.markdown(styled_user_input, unsafe_allow_html=True)
-    st.text(response)
+
+    # Some conditional or callback function process will add to the log
+    st.session_state.log.append(styled_user_input)
+    st.session_state.log.append(response)
+
+    for i, msg in enumerate(st.session_state.log):
+        if i%2 == 0:
+            st.markdown(msg, unsafe_allow_html=True)
+        else:
+            st.text(msg)
