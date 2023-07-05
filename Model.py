5004a3bec593b3741b227287b727720967d652eb
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-07-05 10:44:24 +0200

    Update Model.py

--- Model.py
+++ Model.py
@@ -2,5 +2,5 @@ import random
 
 def model(input):
     i = random.randint(0, 6)
-    risposte = ["Non lo so :D", "Interessante...", "Fantastico!", "Dimmi", "Tutto quello che vuoi", "Mi piaci tanto", "Flora ti amo"]
+    risposte = ["Non lo so :D", "Interessante...", "Fantastico!", "Dimmi", "Tutto quello che vuoi", "OK", "Perfetto"]
     return risposte[i]
