5ec188189a294635672c114d39f0ce23b5257204
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-07-12 13:19:14 +0200

    Update app_old.py

--- app_old.py
+++ app_old.py
@@ -1,30 +1,21 @@
 import streamlit as st
 from Model import model
-from langchain.memory import ConversationBufferWindowMemory
 
 # https://discuss.streamlit.io/t/1-st-button-delete-result-of-2-nd-button/33280/2
 if 'log' not in st.session_state:
     st.session_state.log = []
-
-if 'buffer_memory' not in st.session_state:
-            st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
-
 
 st.title("Chatbot")
 
 user_input = st.text_input("Ask here:")
 
 if st.button("Send"):
-    response = model(user_input, memory = st.session_state.buffer_memory)
+    response = model(user_input)
     styled_user_input = f'<p style="color:green; text-align:right;">{user_input}</p>'
 
     # Some conditional or callback function process will add to the log
     st.session_state.log.append(styled_user_input)
     st.session_state.log.append(response)
-    memory = st.session_state.buffer_memory
-    memory.save_context({"input": user_input}, {"output": response})
-    st.session_state.buffer_memory = memory
-    
 
     for i, msg in enumerate(st.session_state.log):
         if i%2 == 0:
@@ -34,4 +25,3 @@ if st.button("Send"):
 
 if st.button("Clear chat"):
     st.session_state.log = []
-    st.session_state.buffer_memory = ConversationBufferWindowMemory(k=3,return_messages=True)
