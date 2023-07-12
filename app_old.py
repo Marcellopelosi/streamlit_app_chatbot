7e62ceb3cd05988a70e8e0470e48100acbaa1a3f
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-07-12 23:51:00 +0200

    Update app_old.py

--- app_old.py
+++ app_old.py
@@ -3,19 +3,23 @@ from Model import answer
 
 # https://discuss.streamlit.io/t/1-st-button-delete-result-of-2-nd-button/33280/2
 if 'log' not in st.session_state:
-    st.session_state.log = []
+     st.session_state.log = []
+    
+if 'memory' not in st.session_state:
+    st.session_state.memory = ConversationBufferMemory(memory_key="chat_history")
+
 
 st.title("Chatbot")
 
 user_input = st.text_input("Ask here:")
 
 if st.button("Send"):
-    response = answer(user_input)
+    response, actual_memory = answer(user_input, st.session_state.memory)
     styled_user_input = f'<p style="color:green; text-align:right;">{user_input}</p>'
 
-    # Some conditional or callback function process will add to the log
     st.session_state.log.append(styled_user_input)
     st.session_state.log.append(response)
+    st.session_state.memory = actual_memory
 
     for i, msg in enumerate(st.session_state.log):
         if i%2 == 0:
@@ -24,4 +28,6 @@ if st.button("Send"):
             st.text(msg)
 
 if st.button("Clear chat"):
+    st.session_state.memory = ConversationBufferMemory(memory_key="chat_history")
     st.session_state.log = []
+
