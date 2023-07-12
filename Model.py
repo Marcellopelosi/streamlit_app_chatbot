b266c8f13325460e20dd8024b4ae26bbe3358f6d
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-07-12 23:52:17 +0200

    Update Model.py

--- Model.py
+++ Model.py
@@ -10,6 +10,8 @@ from langchain.vectorstores import Chroma
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+
 
 
 
